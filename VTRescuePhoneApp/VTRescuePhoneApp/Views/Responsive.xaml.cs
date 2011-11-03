67040f17cc6f197386b8c6ca3ca863f4afc1e02e
minxmg <minxmg@yahoo.com>
2011-11-03 16:50:04 -0400

    Views
    Signed-off-by: Views
    Signed-off-by: minxmg <minxmg@yahoo.com>

---
+++ VTRescuePhoneApp/VTRescuePhoneApp/Views/Responsive.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace VTRescuePhoneApp.Views
+{
+    public partial class Page1 : PhoneApplicationPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Views/AEDMap.xaml", UriKind.Relative));
+        }
+    }
+}
