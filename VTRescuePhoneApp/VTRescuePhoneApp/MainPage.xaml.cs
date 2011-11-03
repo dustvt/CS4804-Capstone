6a4dc5812d1e42eb472c983eef8e7ed14c33dadf
minxmg <minxmg@yahoo.com>
2011-11-03 16:48:40 -0400

    Signed-off-by: Basic UI minxmg <minxmg@yahoo.com> Signed-off-by: minxmg <minxmg@yahoo.com> Basic UI

--- VTRescuePhoneApp/VTRescuePhoneApp/MainPage.xaml.cs
+++ VTRescuePhoneApp/VTRescuePhoneApp/MainPage.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Tasks;
 
 namespace VTRescuePhoneApp
 {
@@ -23,12 +24,21 @@ namespace VTRescuePhoneApp
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(new Uri("/AEDMap.xaml", UriKind.Relative));
+            NavigationService.Navigate(new Uri("/Views/Responsive.xaml", UriKind.Relative));
+            //button1.IsEnabled = false;
         }
 
         private void button2_Click(object sender, RoutedEventArgs e)
         {
+            NavigationService.Navigate(new Uri("/Views/UnderConstruction.xaml", UriKind.Relative));
+        }
 
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            PhoneCallTask pct = new PhoneCallTask();
+            //pct.DisplayName = "911";
+            pct.PhoneNumber = "911";
+            pct.Show();  
         }
     }
 }
